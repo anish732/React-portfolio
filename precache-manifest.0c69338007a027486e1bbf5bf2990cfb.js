607c967d5852cb89f615592c20833e9e1b1540cb
Shital Patel <shital732@yahoo.com>
2019-12-21 00:17:10 -0500

    Updates

---
+++ precache-manifest.0c69338007a027486e1bbf5bf2990cfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "229f36bb2be7e0098d711a4bc7d9772b",
+    "url": "/React-portfolio/index.html"
+  },
+  {
+    "revision": "3d413c69d95fa1150054",
+    "url": "/React-portfolio/static/css/main.cb846b2a.chunk.css"
+  },
+  {
+    "revision": "3d7f41bfa89f0abf814e",
+    "url": "/React-portfolio/static/js/2.6fe03278.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/React-portfolio/static/js/2.6fe03278.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d413c69d95fa1150054",
+    "url": "/React-portfolio/static/js/main.bce317e9.chunk.js"
+  },
+  {
+    "revision": "3bc550bcde627246fded",
+    "url": "/React-portfolio/static/js/runtime-main.81189ee6.js"
+  },
+  {
+    "revision": "7e1dd4dcf91e83c766fcebfbbebcaba0",
+    "url": "/React-portfolio/static/media/code.7e1dd4dc.png"
+  }
+]);
