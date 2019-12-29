b513f067d7f2363d1b9dd870e3c782fb8653e59e
Shital Patel <shital732@yahoo.com>
2019-12-28 22:54:35 -0500

    Change opacity of bg image

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,13 +5,13 @@ function About() {
   return (
     <div className="about-page" id="about">
       <div className="about-me">
-        <h2>
+        <h3>
           Full Stack Web Developer with an extensive and successful background
           in management. A quick learner who is constantly seeking to improve
           skill set through solving technical challenges and exceeding project
           requirements.Highly adaptable to any situation and thrive in team
           collaborations as well as solo projects.
-        </h2>
+        </h3>
         <br></br>
       </div>
     </div>
